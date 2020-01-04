706f885db5275471b9d4f1e238d0c48a9c4ae183
Kolkos <a.vanderkolk@kolkos.nl>
2020-01-04 13:29:28 +0100

    Fixed mapping fields in response

--- src/main/java/nl/kolkos/domoticz/dashboard/domoticz/models/StatusResult.java
+++ src/main/java/nl/kolkos/domoticz/dashboard/domoticz/models/StatusResult.java
@@ -1,23 +1,40 @@
 package nl.kolkos.domoticz.dashboard.domoticz.models;
 
+import com.google.gson.annotations.SerializedName;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import javax.persistence.Entity;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class StatusResult {
+
+    @SerializedName(value = "Data")
     private String data;
+
+    @SerializedName(value = "Description")
     private String description;
+
+    @SerializedName(value = "idx")
     private int idx;
+
+    @SerializedName(value = "Level")
     private int level;
+
+    @SerializedName(value = "LevelInt")
     private int levelInt;
+
+    @SerializedName(value = "SetPoint")
     private String setPoint;
+
+    @SerializedName(value = "Name")
     private String name;
-    private String LastUpdate;
+
+    @SerializedName(value = "LastUpdate")
+    private String lastUpdate;
+
+    @SerializedName(value = "Status")
     private String status;
 
 }
